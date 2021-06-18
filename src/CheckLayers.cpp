6ae4ffd3cbc1ac6184a24357d34d0cfb823bbb5d
Leo Zhou <zhoul@microsoft.com>
2021-06-18 21:23:43 +0800

    add checklayer

---
+++ src/CheckLayers.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include <assert.h>
+#include <memory>
+#include <mutex>
+
+#include <vulkan/vulkan.h>
+
+static int main() {
+	std::cout << "Hello Vulkan : Checking Layers\n";
+
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+	std::cout << "Get Layers Count = " << layerCount << "\n";
+
+	// get all extension properties
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	// list all available extension properties
+	std::cout << "Available Layers:\n";
+
+	/**
+	* typedef struct VkLayerProperties {
+    *   char        layerName[VK_MAX_EXTENSION_NAME_SIZE];
+    *   uint32_t    specVersion;
+    *   uint32_t    implementationVersion;
+    *   char        description[VK_MAX_DESCRIPTION_SIZE];
+	* } VkLayerProperties;
+	*/
+
+	for (const auto& layer : availableLayers) {
+		std::cout << "-" << layer.layerName << "\n"
+			<< "  - specVersion :" << layer.specVersion  << '\n'
+			<< "  - specVersion :" << layer.implementationVersion << '\n'
+			<< "  - description :" << layer.description << '\n'
+			;
+	}
+
+}
---
+++ src/CheckLogicalDevice.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include <assert.h>
+#include <memory>
+#include <mutex>
+
+#include <vulkan/vulkan.h>
+
+static int main() {
+	std::cout << "Hello Vulkan : Checking Logical Devices\n";
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << "Get Extension Count = " << extensionCount << "\n";
+
+	// get all extension properties
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, 
+											extensions.data());
+
+	// list all available extension properties
+	std::cout << "available extensions:\n";
+
+	for (const auto& extension : extensions) {
+		std::cout << '\t' << extension.extensionName << " : specVersion " << extension.specVersion  << '\n';
+	}
+
+}
